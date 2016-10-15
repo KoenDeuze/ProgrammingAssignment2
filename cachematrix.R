22bd7e2fdb4d7fdbac3285bc9f1d7175e4d85078
Koen Deuze <koendeuze@gmail.com>
2016-10-15 17:31:47 +0200

    first version, just trying

--- cachematrix.R
+++ cachematrix.R
@@ -3,6 +3,16 @@
 
 ## Write a short comment describing this function
 
+## Function "makeCacheMatrix" creates a "matrix" object that is able to cache its inverse. 
+## makeCacheMatrix contains 4 functions: set, get, setinverse, getinverse
+
+## get --> a function that returns the matrix x stored in the main function.
+## set --> a function that changes the matrix stored in the main function.
+
+## setmean and getmean are functions similar to set and get.
+## They don't calculate the matrix, they store the value of the input in a variable m
+## into the main function makeCacheMatrix(setinverse) and return it (getinverse)
+
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
         set <- function(y) {
